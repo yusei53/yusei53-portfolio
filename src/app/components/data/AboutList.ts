e04ed08fb3114bed858b3c9642ba46154b3921b5
yusei53 <yusei530922@gmail.com>
2023-09-08 00:56:16 +0900

    Aboutの型を指定

---
+++ src/app/components/data/AboutList.ts
@@ -0,0 +1,18 @@
+import About from "../models/About";
+
+const AboutList: About[] = [
+  {
+    date: "2018-2021",
+    title: "京華高校入学",
+    subtitle:
+      "硬式テニス推薦で入学/私立大会ダブルス5位/全国私学出場/東京都選抜大会7位",
+  },
+  {
+    date: "2018-2021",
+    title: "京華高校入学",
+    subtitle:
+      "硬式テニス推薦で入学/私立大会ダブルス5位/全国私学出場/東京都選抜大会7位",
+  },
+];
+
+export default AboutList;
---
+++ src/app/components/models/About.ts
@@ -0,0 +1,5 @@
+export default interface About {
+  date: string;
+  title: string;
+  subtitle: string;
+}
