152383e97813d8eaf5a7709347e7340faea28c9e
yusei53 <yusei530922@gmail.com>
2023-09-08 01:44:13 +0900

    data更新

--- src/app/components/data/AboutList.ts
+++ src/app/components/data/AboutList.ts
@@ -2,16 +2,72 @@ import About from "../models/About";
 
 const AboutList: About[] = [
   {
-    date: "2018-2021",
-    title: "京華高校入学",
-    subtitle:
-      "硬式テニス推薦で入学/私立大会ダブルス5位/全国私学出場/東京都選抜大会7位",
+    date: "2009-2012",
+    title: "大泉学園桜小学校入学",
+    subtitle: "小学1~3年生/バスケ得意/人を笑わせることが好き",
+  },
+  {
+    date: "2012-2015",
+    title: "中国 広州日本人学校に転校",
+    subtitle: "小学4~6年生/テニスを始める/アジアの5カ国に旅行",
+  },
+  {
+    date: "2015-2018",
+    title: "大泉学園桜中学校入学",
+    subtitle: "テニスで都大会に出場/人生のターニングポイント[1]",
   },
   {
     date: "2018-2021",
     title: "京華高校入学",
     subtitle:
-      "硬式テニス推薦で入学/私立大会ダブルス5位/全国私学出場/東京都選抜大会7位",
+      "スポーツ推薦で入学/私立大会ダブルス5位/全国私学出場/東京都選抜大会7位",
+  },
+  {
+    date: "2021-now",
+    title: "成蹊大学経済学部入学",
+    subtitle: "テニスコーチ始める/プログラミングに出会う",
+  },
+  {
+    date: "2021/12",
+    title: "プログラミングを始める",
+    subtitle:
+      "知らないおじさんに話しかけられる/Techgymというプログラミングスクールにjoin/python",
+  },
+  {
+    date: "2022/9-now",
+    title: "株式会社CloudQにjoin",
+    subtitle: "エンジニアインターンスタート/html,css,js,phpの実務経験",
+  },
+  {
+    date: "2022/12-now",
+    title: "成蹊大学プログラミング団体Peach.techにjoin",
+    subtitle:
+      "知らない先輩に話しかけられる/現在サークル化, 代表を務める/プログラミングの面白さを知る",
+  },
+  {
+    date: "2023/7",
+    title: "ハッカソンで企業賞を受賞",
+    subtitle: "ChatWork株式会社様から企業賞受賞",
+  },
+  {
+    date: "2023/7-now",
+    title: "株式会社Leapode設立",
+    subtitle: "株式会社Leapode代表取締役",
+  },
+  {
+    date: "2023/7-now",
+    title: "株式会社キーザンキーザンにjoin",
+    subtitle: "サマー,長期インターン/next.js, typescriptの実務経験",
+  },
+  {
+    date: "2023/8",
+    title: "株式会社speeeでインターン",
+    subtitle: "12daysのサマーインターン/初rails/事業を作る難しさを知る",
+  },
+  {
+    date: "2033/9",
+    title: "Sansan株式会社でインターン",
+    subtitle: "2weekのハッカソン(サマーインターン)/未来のインフラを作る",
   },
 ];
 
