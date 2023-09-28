60b51480a4a1717af23944068d2b0c822d327419
yusei53 <yusei530922@gmail.com>
2023-09-28 15:45:07 +0900

    supabaseをコメントアウトでコミット

---
+++ src/app/utils/supabaseFunction.ts
@@ -0,0 +1,6 @@
+// import { supabase } from "./supabase";
+
+// export const getAllAboutLists = async () => {
+//   const aboutLists = await supabase.from("aboutList").select("*");
+//   return aboutLists.data;
+// };
