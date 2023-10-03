dacec3c4d4e37533d3b00ee2b711a199df549f3e
yusei53 <yusei530922@gmail.com>
2023-10-03 20:01:46 +0900

    アニメーションの追加を一旦コメントアウトでmerge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,36 @@
 "use client";
 
-import { Footer } from "./components/organisms/Footer";
-import { Header } from "./components/organisms/Header";
+import { useEffect, useState } from "react";
 import { Introduction } from "./components/organisms/Introduction";
 import TabPanel from "./components/organisms/TabPanel";
+import Header from "./components/organisms/Header";
+import { Footer } from "./components/organisms/Footer";
+// import Animation from "./components/molecule/Animation";
 
 export default function Home() {
+  // const [showText, setShowText] = useState(true);
+
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setShowText(false);
+  //   }, 2500);
+
+  //   return () => {
+  //     clearTimeout(timer);
+  //   };
+  // }, []);
+
   return (
     <>
-      <Introduction />
-      <TabPanel />
+      {/* {showText && <Animation text="ゆせのportfolio" />}
+      {!showText && ( */}
+      <>
+        <Header />
+        <Introduction />
+        <TabPanel />
+        <Footer />
+      </>
+      {/* )} */}
     </>
   );
 }
