ebe5853cd905a0c450fff1e7a74828d0f02fb3f4
yusei53 <yusei530922@gmail.com>
2023-10-05 01:05:34 +0900

    アニメーション反映

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,32 +5,51 @@ import { Introduction } from "./components/organisms/Introduction";
 import TabPanel from "./components/organisms/TabPanel";
 import Header from "./components/organisms/Header";
 import { Footer } from "./components/organisms/Footer";
-// import Animation from "./components/molecule/Animation";
+import Animation from "./components/molecule/Animation";
+import { keyframes, styled } from "@mui/system";
 
 export default function Home() {
-  // const [showText, setShowText] = useState(true);
+  const [showText, setShowText] = useState(true);
+  const [pageLoaded, setPageLoaded] = useState(false);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setShowText(false);
-  //   }, 2500);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowText(false);
+      setPageLoaded(true);
+    }, 4000);
 
-  //   return () => {
-  //     clearTimeout(timer);
-  //   };
-  // }, []);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+  const WelcomeContainer = styled("div")({
+    animation: `${fadeIn} 2s ease`,
+    /* 他のスタイルプロパティを追加できます */
+  });
 
   return (
     <>
-      {/* {showText && <Animation text="ゆせのportfolio" />}
-      {!showText && ( */}
-      <>
-        <Header />
-        <Introduction />
-        <TabPanel />
-        <Footer />
-      </>
-      {/* )} */}
+      {showText && <Animation text="ゆせのportfolio☺︎" />}
+      {!showText && pageLoaded && (
+        <>
+          <WelcomeContainer>
+            <Header />
+            <Introduction />
+            <TabPanel />
+            <Footer />
+          </WelcomeContainer>
+        </>
+      )}
     </>
   );
 }
